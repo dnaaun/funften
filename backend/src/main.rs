6488e1d2f9de615e60f4dda42edd9d7bded1d791
davidatsurge <93471693+davidatsurge@users.noreply.github.com>
2023-04-26 22:41:01 +0300

    more basic setup

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use tower_http::trace::TraceLayer;
+use tower_http::cors::CorsLayer;
+use std::net::SocketAddr;
+
+use axum::{Router, http::HeaderValue};
+use tracing::debug;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let router = Router::new();
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 2001));
+    debug!("Listening on {}", addr);
+
+    let cors_layer = CorsLayer::new()
+        // allow `GET` and `POST` when accessing the resource
+        .allow_methods(tower_http::cors::Any)
+        .allow_headers(tower_http::cors::Any)
+        .allow_origin("http://localhost:1001".parse::<HeaderValue>().unwrap());
+
+    let trace_layer = TraceLayer::new_for_http();
+
+    let router = router.layer(cors_layer).layer(trace_layer);
+
+
+    axum::Server::bind(&addr)
+        .serve(router.into_make_service())
+        .await
+        .expect("serving");
 }
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,17 @@
+use frontend::SimpleCounterProps;
+use frontend::SimpleCounter;
+use leptos::*;
+
 fn main() {
-    println!("Hello, world!");
+    console_error_panic_hook::set_once();
+    tracing_wasm::set_as_global_default();
+
+    mount_to_body(|cx| {
+        view! { cx,
+            <SimpleCounter
+                initial_value=0
+                step=1
+            />
+        }
+    })
 }
