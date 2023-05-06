d9f84ac14dc49e4c5cdbf2a156265ef5d0e7dad6
davidatsurge <93471693+davidatsurge@users.noreply.github.com>
2023-05-06 09:56:37 +0300

    button styling

--- rust/frontend/src/components/button.rs
+++ rust/frontend/src/components/button.rs
@@ -9,6 +9,6 @@ pub fn Button<V: IntoView + std::clone::Clone>(
     button(cx)
         .prop(
         "type","button")
-        .classes("text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800")
+        .classes("text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800")
         .child(text)
 }
