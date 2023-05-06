33d656044ee68ecd42ce4da0d4c5626ebde69a5a
davidatsurge <93471693+davidatsurge@users.noreply.github.com>
2023-05-06 09:30:29 +0300

    write code I don't want to use

---
+++ rust/frontend/src/leptos_utils/converted_signal.rs
@@ -0,0 +1,29 @@
+use leptos::{*, tracing::info};
+
+/// This uses effects, but not to syncrhonize state with external
+/// things like APIs. So I want to avoid it.
+pub fn create_converted_signal<T, U>(
+    cx: Scope,
+    read: ReadSignal<T>,
+    write: WriteSignal<T>,
+    convert_to: impl Fn(T) -> U + 'static,
+    convert_from: impl Fn(U) -> T + 'static,
+) -> RwSignal<U>
+where
+    T: Clone,
+    U: Clone,
+{
+    let converted_signal = create_rw_signal(cx, convert_to(read.get()));
+
+    create_effect(cx, |_| {
+        info!("converting from");
+        write(convert_from(converted_signal.get()));
+    });
+
+    create_effect(cx, |_| {
+        info!("converting to");
+        converted_signal.set(convert_to(read.get()));
+    });
+
+    converted_signal
+}
